f10fa4fd15db82007868e532d8323419c022422f
Jackie <JGtheophila19@gmail.com>
2019-12-21 23:47:18 -0500

    using variables and constants

--- CsharpFundamentals/Program.cs
+++ CsharpFundamentals/Program.cs
@@ -1,17 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-/*using statement*/
+using System;
 
-namespace HelloWorld
+namespace CsharpFundamentels
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World");
-        }
-    }
-}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// byte number;
+			//Console.WriteLine(number);
+		    byte number = 2;
+
+			int count = 10;
+			float totalPrice = 20.95f;
+			
+			char character = 'A';
+			string firstName = 'Mosh';
+			bool isWorking = false;
+			
+			Console.WriteLine(number);
+			Console.WriteLine(count);
+			Console.WriteLine(totalPrice);
+			Console.WriteLine(character);
+			Console.WriteLine(firstName);
+			Console.WriteLine(isWorking);
+		}
+	}
