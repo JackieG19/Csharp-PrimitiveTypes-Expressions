0f07fcfb7a69530e7621012a23068b8f6836deab
Jackie <JGtheophila19@gmail.com>
2019-12-23 14:28:04 -0500

    converting a byte into an integer

--- CsharpFundamentals/Program.cs
+++ CsharpFundamentals/Program.cs
@@ -6,22 +6,9 @@ namespace CsharpFundamentels
 	{
 		static void Main(string[] args)
 		{
-// 			byte b = 1;
-// 			int i = b;
-// 			Console.WriteLine(i);
-			
-// 			int i = 1000;
-// 			byte b = (byte) i;
-// 			Console.WriteLine(b);
-			
-			var number = "1234";
-			//int i = (int) number;
-			
-			byte b = Convert.ToInt32(number);
+			byte b = 1;
+			int i = b;
 			Console.WriteLine(i);
-			
-			// the app will crash = System.OverflowException
-			// byte b = Convert.ToByte(number);
-			// Console.WriteLine(b);
+
 		}
 	}
