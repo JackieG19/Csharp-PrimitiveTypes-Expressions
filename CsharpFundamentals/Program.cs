d88ad6a31a8e6a6cfc365675e9f0a9205ef7f753
Jackie <JGtheophila19@gmail.com>
2019-12-23 14:36:28 -0500

    using Convert.ToByte

--- CsharpFundamentals/Program.cs
+++ CsharpFundamentals/Program.cs
@@ -6,15 +6,17 @@ namespace CsharpFundamentels
 	{
 		static void Main(string[] args)
 		{
-			var number = "1234";
-			//int i = (int) number;
+			try
+			{
+				var number = "1234"
+				byte b = Convert.ToByte(number);
+				Console.WriteLine(b);
+			}
 			
-			byte b = Convert.ToInt32(number);
-			Console.WriteLine(i);
-			
-			// the app will crash = System.OverflowException
-			//byte b = Convert.ToByte(number);
-			//Console.WriteLine(b);
+			catch (Exception)
+			{
+				Console.WriteLine("The number could not be converted to a byte");		
+			}
 
 		}
 	}
