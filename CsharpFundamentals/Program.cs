391adc4ffe302abae702fed4ae0e2128904debd7
Jackie <JGtheophila19@gmail.com>
2019-12-23 14:32:38 -0500

    converting a string into a number

--- CsharpFundamentals/Program.cs
+++ CsharpFundamentals/Program.cs
@@ -6,9 +6,15 @@ namespace CsharpFundamentels
 	{
 		static void Main(string[] args)
 		{
-			int i = 1000;
-			byte b = (byte) i;
-			Console.WriteLine(b);
+			var number = "1234";
+			//int i = (int) number;
+			
+			byte b = Convert.ToInt32(number);
+			Console.WriteLine(i);
+			
+			// the app will crash = System.OverflowException
+			//byte b = Convert.ToByte(number);
+			//Console.WriteLine(b);
 
 		}
 	}
